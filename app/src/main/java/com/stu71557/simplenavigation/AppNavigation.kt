cd6d47dd08238d998697539d4b5ee5b915a706d5
Juan Nascimento <juannascimento048@gmail.com>
2024-04-04 18:24:33 -0300

    add movie list and screen to show movie details

--- app/src/main/java/com/stu71557/simplenavigation/AppNavigation.kt
+++ app/src/main/java/com/stu71557/simplenavigation/AppNavigation.kt
@@ -1,28 +1,47 @@
 package com.stu71557.simplenavigation
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
 @Composable
-fun AppNavigation() {
-    val navController = rememberNavController()
+fun AppNavigation(
+    navController: NavHostController = rememberNavController(), paddingValues: PaddingValues
+) {
+    val viewModel: MovieViewModel = viewModel()
+    val movies = viewModel.movies
 
     NavHost(
         navController = navController,
         startDestination = Routes.FirstScreen.route,
     ) {
-        composable(route = Routes.FirstScreen.route)
-        {
-            Screen1(navController = navController)
+        composable(route = Routes.FirstScreen.route) {
+            Screen1(movies = movies,
+                paddingValues = paddingValues,
+                onNavigateToSecondScreen = { movie ->
+                    val movieId = movie.id
+                    navController.navigate("${Routes.SecondScreen.route}/$movieId")
+                })
         }
-        composable(route = Routes.SecondScreen.route)
-        {
-            Screen2(navController = navController)
+        // Define the composable for Screen2 with a dynamic parameters (movieId)
+        composable(route = "${Routes.SecondScreen.route}/{movieId}",
+            arguments = listOf(navArgument("movieId") {
+                type = NavType.IntType
+            })) { backStackEntry ->
+            val movieId = backStackEntry.arguments?.getInt("movieId")!!
+            val movie = viewModel.getMovieById(movieId)
+            Screen2(
+                movie = movie,
+                paddingValues = paddingValues
+            )
         }
-        composable(route = Routes.ThirdScreen.route)
-        {
+        composable(route = Routes.ThirdScreen.route) {
             Screen3(navController = navController)
         }
     }
